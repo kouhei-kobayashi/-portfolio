ff6a54a25086cea1328364175964e3500a74aa56
kouhei-kobayashi <kouheimac3@gmail.com>
2020-07-10 01:00:01 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+$(window).on("scroll",function(){
+//    fadeIn
+$('[data-fadeIn]').each(function(index,el){
+    if($(window).scrollTop() > $(el).offset().top - $(window).height() / 2){
+        $(el).addClass('is-over');
+      
+    }
+}
+)
+//トップページへ戻る
+if($(window).scrollTop() > 100){
+    $('.cercle-baner').addClass('is-over');
+}else{
+    $('.cercle-baner').removeClass('is-over');
+}
+
+
+})
